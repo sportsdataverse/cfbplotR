3ca3a71371b0dcad05a48a5582ad3e17037893b7
Kazink36 <13jaredlee@gmail.com>
2021-09-07 22:46:55 -0700

    Fixed namespace for mean_geom

--- R/middle_geoms.R
+++ R/middle_geoms.R
@@ -1,6 +1,9 @@
-#' Plot Middle Lines
+#' @name middle_geoms
+#' @aliases median_geom mean_geom
+#' @title
+#' **Plot Middle Lines**
 #'
-#' These functions plot lines representing the median or mean of the data for ggplot. From Thomas Mock (https://github.com/nflverse/nflplotR/issues/1)
+#' @description These functions plot lines representing the median or mean of the data for ggplot. From Thomas Mock (https://github.com/nflverse/nflplotR/issues/1)
 #'
 #' @param x The X data
 #' @param y The Y data
@@ -27,7 +30,12 @@ median_geom <- function(x,y, color = "red", linetype = "dashed"){
     ggplot2::geom_hline(ggplot2::aes(yintercept = median({{y}})), color = color, linetype = linetype)
   )
 }
-
+#' @rdname middle_geoms
+#' @param x The X data
+#' @param y The Y data
+#' @param color The color of the line
+#' @param linetype The linetype of the line
+#' @export
 mean_geom <- function(x,y, color = "red", linetype = "dashed"){
   list(
     ggplot2::geom_vline(ggplot2::aes(xintercept = mean({{x}})), color = color, linetype = linetype),
