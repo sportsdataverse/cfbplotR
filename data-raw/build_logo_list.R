7edae848ee431a67abe55d75a92f217d2377e93c
Kazink36 <13jaredlee@gmail.com>
2021-09-07 21:48:08 -0700

    Made logo data

---
+++ data-raw/build_logo_list.R
@@ -0,0 +1,30 @@
+team_info <- cfbfastR::cfbd_team_info(only_fbs = FALSE  )
+
+
+temp_logos <-team_info %>%
+  unnest_wider(logos,names_sep = "_") %>%
+  select(school,abbreviation,logo = logos_1) %>%
+  pivot_longer(school:abbreviation,values_to = "school") %>%
+  select(-name) %>%
+  group_by(school) %>%
+  slice(1) %>%
+  ungroup()
+
+conferences <- cfbfastR::cfbd_conferences() %>%
+  mutate(logo = glue::glue("https://a.espncdn.com/i/teamlogos/ncaa_conf/500/{conference_id}.png"))
+temp_conf <- conferences %>%
+  pivot_longer(c(name,abbreviation),values_to = "school") %>%
+  select(-long_name,-conference_id,-name) %>%
+  filter(!is.na(school)) %>%
+  group_by(school) %>%
+  slice(1) %>%
+  ungroup()
+
+combined <- bind_rows(temp_logos,temp_conf)
+
+logo_list <- combined$logo
+names(logo_list) <- combined$school
+
+logo_list
+
+usethis::use_data(logo_list)
