2cda7e4546f0ee4332c9937cb37a9de9ccb8301f
Kazink36 <13jaredlee@gmail.com>
2021-09-07 22:17:40 -0700

    Added middle geoms

---
+++ R/middle_geoms.R
@@ -0,0 +1,38 @@
+#' Plot Middle Lines
+#'
+#' These functions plot lines representing the median or mean of the data for ggplot. From Thomas Mock (https://github.com/nflverse/nflplotR/issues/1)
+#'
+#' @param x The X data
+#' @param y The Y data
+#' @param color The color of the line
+#' @param linetype The linetype of the line
+#' @export
+#'
+#'
+#' @examples
+#' \donttest{
+#' ggplot(mtcars) +
+#'  geom_point(aes(x = disp, y = mpg)) +
+#'  median_geom(disp, mpg)
+#'
+#'
+#' ggplot(mtcars) +
+#'  geom_point(aes(x = disp, y = mpg)) +
+#'  mean_geom(disp, mpg)
+#'  }
+
+median_geom <- function(x,y, color = "red", linetype = "dashed"){
+  list(
+    ggplot2::geom_vline(ggplot2::aes(xintercept = median({{x}})), color = color, linetype = linetype),
+    ggplot2::geom_hline(ggplot2::aes(yintercept = median({{y}})), color = color, linetype = linetype)
+  )
+}
+
+mean_geom <- function(x,y, color = "red", linetype = "dashed"){
+  list(
+    ggplot2::geom_vline(ggplot2::aes(xintercept = mean({{x}})), color = color, linetype = linetype),
+    ggplot2::geom_hline(ggplot2::aes(yintercept = mean({{y}})), color = color, linetype = linetype)
+  )
+}
+
+
