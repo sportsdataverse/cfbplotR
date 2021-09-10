dae114e6740c84057c3862e454c37c53e3c0828e
Kazink36 <13jaredlee@gmail.com>
2021-09-09 21:51:59 -0700

    added FCS teams

--- data-raw/build_logo_list.R
+++ data-raw/build_logo_list.R
@@ -1,19 +1,60 @@
-team_info <- cfbfastR::cfbd_team_info(only_fbs = FALSE  )
+# team_info <- cfbfastR::cfbd_team_info(only_fbs = FALSE  )
+#
+#
+# temp_logos <-team_info %>%
+#   unnest_wider(logos,names_sep = "_") %>%
+#   select(school,abbreviation,logo = logos_1) %>%
+#   pivot_longer(school:abbreviation,values_to = "school") %>%
+#   select(-name) %>%
+#   group_by(school) %>%
+#   slice(1) %>%
+#   ungroup()
+#
 
 
-temp_logos <-team_info %>%
-  unnest_wider(logos,names_sep = "_") %>%
-  select(school,abbreviation,logo = logos_1) %>%
+#
+#
+#
+# logo_list <- lapply(combined$school, function(x){
+#   url <- combined$logo[combined$school == x]
+#   curl::curl_fetch_memory(url)$content
+# })
+# logo_list <- rlang::set_names(logo_list, combined$school)
+# #logo_list <- combined$logo
+# #names(logo_list) <- combined$school
+#
+# logo_list
+#
+# usethis::use_data(logo_list, internal = TRUE, overwrite = TRUE)
+#
+
+
+
+
+
+
+
+
+
+fbs_teams <- cfbfastR::cfbd_team_info()
+teams_csv <- read_csv("https://raw.githubusercontent.com/CFBNumbers/logos/main/cfblogos.csv",skip = 1,col_names = c("row","team_id","school","mascot","abbreviation","alt_name1","alt_name2","alt_name3","conference","division","color","alt_color","logo","logo_dark"))
+
+temp_logos <- teams_csv %>%
+  select(school, abbreviation, logo) %>%
+  filter(logo != "NA,NA") %>%
+  mutate(fbs = if_else(school %in% fbs_teams$school,1,2)) %>%
   pivot_longer(school:abbreviation,values_to = "school") %>%
-  select(-name) %>%
   group_by(school) %>%
   slice(1) %>%
-  ungroup()
+  ungroup() %>%
+  arrange(fbs,desc(name)) %>%
+  select(-name,-fbs)
 
 conferences <- cfbfastR::cfbd_conferences()
 conferences<- conferences %>%
   mutate(logo = glue::glue("https://a.espncdn.com/i/teamlogos/ncaa_conf/500/{conference_id}.png")) %>%
   filter(conference_id<100)
+
 temp_conf <- conferences %>%
   pivot_longer(c(name,abbreviation),values_to = "school") %>%
   select(-long_name,-conference_id,-name) %>%
@@ -24,14 +65,7 @@ temp_conf <- conferences %>%
 
 combined <- bind_rows(temp_logos,temp_conf)
 
-logo_list <- lapply(combined$school, function(x){
-  url <- combined$logo[combined$school == x]
-  curl::curl_fetch_memory(url)$content
-})
-logo_list <- rlang::set_names(logo_list, combined$school)
-#logo_list <- combined$logo
-#names(logo_list) <- combined$school
-
-logo_list
+logo_list <- combined$logo
+names(logo_list) <- combined$school
 
 usethis::use_data(logo_list, internal = TRUE, overwrite = TRUE)
