2e109be876e3a3e85b8bafa2c7ba155db74f3e17
saiemgilani <saiem.gilani@gmail.com>
2023-04-17 10:27:03 -0400

    Matching mlbplotR with some adjustments.. needs review

--- R/gt_fmt_cfb.R
+++ R/gt_fmt_cfb.R
@@ -34,7 +34,7 @@
 #'  player = c("Britain Covey", "Cameron Rising","Non.Match"),
 #'  team = c("Utah","Utah","BYU")
 #') %>%
-#'  cfbplotR::add_athlete_id_col(player)
+#'  add_athlete_id_col(player)
 #'table_2 <- df %>%
 #'  gt() %>%
 #'  gt_fmt_cfb_headshot(athlete_id)
@@ -44,43 +44,16 @@
 #' \if{html}{\figure{fmt_cfb_2.png}{options: width=30\%}}
 #'
 
-
 gt_fmt_cfb_logo <- function(gt_object, columns, height = 30){
 
-  # convert tidyeval column to bare string
-  column_names <- gt:::resolve_cols_c(
-    expr = {{ columns }},
-    data = gt_object
+  stopifnot("'gt_object' must be a 'gt_tbl', have you accidentally passed raw data?" = "gt_tbl" %in% class(gt_object))
+
+  gt_cfbplotR_image(
+    gt_object = gt_object,
+    columns = columns,
+    height = height,
+    type = "cfb_logo"
   )
-
-  stub_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]]=="stub")]
-  grp_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]]=="row_group")]
-
-  # stopifnot("img_source must be 'web' or 'local'" = img_source %in% c("web", "local"))
-  img_source <- "web"
-
-  # need to correct for rownames
-  gt_object %>%
-    gt::text_transform(
-      locations = if(isTRUE(grp_var %in% column_names)){
-        gt::cells_row_groups()
-      } else if(isTRUE(stub_var %in% column_names)){
-        gt::cells_stub(rows = gt::everything())
-      } else {
-        gt::cells_body({{ columns }})
-      },
-      fn = function(x){
-        if(img_source == "web"){
-          #Fix Texas A&M
-          x <- sub("&amp;","&",x)
-          x <- cfbplotR::clean_school_names(as.character(x))
-          x[which(!x%in%valid_team_names())] <- "NCAA"
-          gt::web_image(url = logo_list[x], height = height)
-        } else {
-          gt::local_image(filename = x, height = height)
-        }
-      }
-    )
 
 }
 
@@ -90,34 +63,104 @@ gt_fmt_cfb_logo <- function(gt_object, columns, height = 30){
 
 gt_fmt_cfb_wordmark <- function(gt_object, columns, height = 30) {
 
-  column_names <- gt:::resolve_cols_c(
-    expr = {{ columns }},
-    data = gt_object
+  stopifnot("'gt_object' must be a 'gt_tbl', have you accidentally passed raw data?" = "gt_tbl" %in% class(gt_object))
+
+  gt_cfbplotR_image(
+    gt_object = gt_object,
+    columns = columns,
+    height = height,
+    type = "wordmark"
   )
 
-  stub_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]]=="stub")]
-  grp_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]]=="row_group")]
+}
 
-  # need to correct for rownames
-  gt_object %>%
-    gt::text_transform(
-      locations = if(isTRUE(grp_var %in% column_names)){
-        gt::cells_row_groups()
-      } else if(isTRUE(stub_var %in% column_names)){
-        gt::cells_stub(rows = gt::everything())
-      } else {
-        gt::cells_body({{ columns }})
-      },
-      fn = function(x){
-        #Fix Texas A&M
-        x <- sub("&amp;","&",x)
-        x <- cfbplotR::clean_school_names(as.character(x))
-        x[which(!x%in%names(wordmark_list))] <- "NCAA"
-        gt::web_image(url = wordmark_list[x], height = height)
+
+
+
+# Taken from nflplotR package and adapted for CFB purposes
+gt_cfbplotR_image <- function(gt_object,
+                              columns,
+                              height = 30,
+                              type = c("cfb_logo", "wordmark")){
+
+  rlang::check_installed("gt (>= 0.8.0)", "to render images in gt tables.")
+
+  type <- match.arg(type)
+
+  locations <- gt::cells_body({{ columns }})
+  column_names <- gt::cells_body({{ columns }})
+
+  stub_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]] == "stub")]
+  grp_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]] == "row_group")]
+
+  if (is.numeric(height)) {
+    height <- paste0(height, "px")
+  }
+
+  wordmark_function <- function(x) {
+    #Fix Texas A&M
+    x <- sub("&amp;","&",x)
+    x <- clean_school_names(as.character(x), keep_non_matches = TRUE)
+    x[which(!(x %in% names(wordmark_list)))] <- "NCAA"
+    gt::web_image(url = wordmark_list[x], height = height)
+  }
+
+  cfb_logo_function <- function(x) {
+    img_source <- "web"
+    if (img_source == "web") {
+      #Fix Texas A&M
+      x <- sub("&amp;","&",x)
+      x <- clean_school_names(as.character(x), keep_non_matches = TRUE)
+      x[which(!(x %in% valid_team_names()))] <- "NCAA"
+      gt::web_image(url = logo_list[x], height = height)
+    } else {
+      gt::local_image(filename = x, height = height)
+    }
+  }
+
+  gt::text_transform(
+    data = gt_object,
+    locations = if (isTRUE(grp_var %in% column_names)) {
+      gt::cells_row_groups()
+    } else if (isTRUE(stub_var %in% column_names)) {
+      gt::cells_stub(rows = gt::everything())
+    } else {
+      gt::cells_body({{ columns }})
+    },
+    fn = function(x){
+      if (type == "cfb_logo") {
+        cfb_logo_function(x)
+      } else if (type == "wordmark") {
+        wordmark_function(x)
       }
-    )
+    }
+  )
 
 }
+
+# Taken from gt and nflplotR package
+# Get image URIs from image lists as a vector Base64-encoded image strings
+#' @importFrom base64enc base64encode
+get_image_uri <- function(team_abbr, type = c("cfb_logo", "wordmark")) {
+
+  lookup_list <- switch(type,
+                        "cfb_logo" = logo_list,
+                        "wordmark" = wordmark_list
+  )
+
+  vapply(
+    team_abbr,
+    FUN.VALUE = character(1),
+    USE.NAMES = FALSE,
+    FUN = function(school) {
+      paste0(
+        "data:", "image/png",
+        ";base64,", base64enc::base64encode(lookup_list[[school]])
+      )
+    }
+  )
+}
+
 
 
 #' @rdname gt_cfb
@@ -125,44 +168,42 @@ gt_fmt_cfb_wordmark <- function(gt_object, columns, height = 30) {
 
 gt_fmt_cfb_headshot <- function(gt_object, columns, height = 30) {
 
-  column_names <- gt:::resolve_cols_c(
-    expr = {{ columns }},
-    data = gt_object
-  )
+  stopifnot("'gt_object' must be a 'gt_tbl', have you accidentally passed raw data?" = "gt_tbl" %in% class(gt_object))
 
-  stub_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]]=="stub")]
-  grp_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]]=="row_group")]
+  rlang::check_installed("gt (>= 0.8.0)", "to render images in gt tables.")
+
+  column_names <- gt::cells_body({{ columns }})
+
+  stub_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]] == "stub")]
+  grp_var <- gt_object[["_boxhead"]][["var"]][which(gt_object[["_boxhead"]][["type"]] == "row_group")]
+
+  if (is.numeric(height)) {
+    height <- paste0(height, "px")
+  }
 
   # need to correct for rownames
-  gt_object %>%
-    gt::text_transform(
-      locations = if(isTRUE(grp_var %in% column_names)){
-        gt::cells_row_groups()
-      } else if(isTRUE(stub_var %in% column_names)){
-        gt::cells_stub(rows = gt::everything())
-      } else {
-        gt::cells_body({{ columns }})
-      },
-      fn = function(x){
-        for (i in 1:length(x)) {
-          url_or_id <- headshot_id_or_url(x[i])
-          if(url_or_id == "ID") {
-            x[i] <- headshot_id_to_url(x[i])
-          } else if(url_or_id == "NA"){
-            x[i] <- "http://a.espncdn.com/i/headshots/nophoto.png"
-          }
+  gt::text_transform(
+    data = gt_object,
+    locations = if (isTRUE(grp_var %in% column_names)) {
+      gt::cells_row_groups()
+    } else if (isTRUE(stub_var %in% column_names)) {
+      gt::cells_stub(rows = gt::everything())
+    } else {
+      gt::cells_body({{ columns }})
+    },
+    fn = function(x){
+      for (i in 1:length(x)) {
+        url_or_id <- headshot_id_or_url(x[i])
+        if (url_or_id == "ID") {
+          x[i] <- headshot_id_to_url(x[i])
+        } else if (url_or_id == "NA") {
+          x[i] <- "http://a.espncdn.com/i/headshots/nophoto.png"
         }
-
-        if (is.numeric(height)) {
-          height <- paste0(height, "px")
-        }
-
-        x <- paste0("<img src=\"", x, "\" style=\"height:", height,
-                    ";\" onerror=\"this.onerror=null;this.src='http://a.espncdn.com/i/headshots/nophoto.png';\" />")
       }
-    )
+
+      x <- paste0("<img src=\"", x, "\" style=\"height:", height,
+                  ";\" onerror=\"this.onerror=null;this.src='http://a.espncdn.com/i/headshots/nophoto.png';\" />")
+    }
+  )
 
 }
-
-
-
